f27615fee4f34e1d27b7676c392d3d81fcb51a94
Tim Smart <tim.smart@arisechurch.com>
2023-01-21 12:51:06 +1300

    refactor: multipart api

--- packages/core/src/multipart.ts
+++ packages/core/src/multipart.ts
@@ -2,6 +2,7 @@ export type FormDataPart = FormDataField | FormDataFile
 
 export class FormDataField {
   readonly _tag = "FormDataField"
+
   constructor(
     readonly key: string,
     readonly contentType: string,
@@ -11,11 +12,16 @@ export class FormDataField {
 
 export class FormDataFile {
   readonly _tag = "FormDataFile"
+
   constructor(
     readonly key: string,
     readonly name: string,
     readonly contentType: string,
     readonly size: number,
-    readonly content: Stream<never, ReadableStreamError, Uint8Array>,
+    readonly content: ReadableStream<Uint8Array>,
   ) {}
+
+  get stream() {
+    return fromReadableStream(this.content)
+  }
 }
