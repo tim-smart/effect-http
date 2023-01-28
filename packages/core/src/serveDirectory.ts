e78abc01983660a8c1da83720501d4b3aa1c7b74
Tim Smart <tim.smart@arisechurch.com>
2023-01-28 21:38:18 +1300

    fix: prevent path escalation in serveDirectory

--- packages/core/src/serveDirectory.ts
+++ packages/core/src/serveDirectory.ts
@@ -1,11 +1,12 @@
 import * as Mime from "mime-types"
 import parseRange from "range-parser"
+import * as Path from "node:path"
 
 export const serveDirectory =
   (directory: string): HttpApp<HttpFs, HttpFsNotFound | HttpFsError> =>
   (request) => {
     const url = new URL(request.url)
-    const path = `${directory}${url.pathname}`
+    const path = Path.join(directory, Path.join("/", url.pathname))
     const contentType = Mime.lookup(path)
 
     const range = request.headers.get("range")
