204017b5a1a6089953fa75a248444fee5beabf8a
Tim Smart <hello@timsmart.co>
2023-02-10 11:43:00 +1300

    chore: export executor

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./Request.js"
 export * from "./Request/FetchExecutor.js"
 export * from "./Error.js"
-export * as response from "./Response.js"
+
 export * as body from "./Request/Body.js"
+export * as executor from "./Request/Executor.js"
+export * as response from "./Response.js"
